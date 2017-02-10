baa9b88fd0ff5a8e41e7bacf4c4ba19f8f322f55
maxgfr <maxgolfier@gmail.com>
2017-02-09 23:27:17 -0500

    :+1:

---
+++ app/src/main/java/com/maxgfr/webviewmax/MainActivity.java
@@ -0,0 +1,49 @@
+package com.maxgfr.webviewmax;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.wview);
+
+        //Tester la connexion avant de lancer la webview
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        if (networkInfo == null || networkInfo.isConnected() == false) {
+            webView.loadData("<h1>Problème de connexion internet...<h1>", "text/html", null);
+            return;
+        }
+
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.creatb.gq/");
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (webView.canGoBack() == true) {
+                webView.goBack();
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+}
